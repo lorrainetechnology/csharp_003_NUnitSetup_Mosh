1d95325585ac24853d48e9e6823d4dfd1c465c75
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-15 14:31:30 +0530

    NUnit Setup

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+        [SetUp]
+        public void Setup()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {        
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+            var result = _math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            var result = _math.Max(1,2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnSameArgument()
+        {
+            var result = _math.Max(1, 1);
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
